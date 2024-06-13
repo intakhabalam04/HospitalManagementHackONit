5b9f6d81742a3b0c78e10c0cf334541cfe606d36
purna9547 <pradhan.purna2017@gmail.com>
2024-06-14 03:09:32 +0530

    change the fragments of patient

--- src/main/resources/static/js/Patient/fragments.js
+++ src/main/resources/static/js/Patient/fragments.js
@@ -4,3 +4,12 @@ document.addEventListener("DOMContentLoaded", function() {
 function logout() {
     window.location.href = "/logout";
 }
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('show');
+});
+
+document.querySelectorAll('.sidebar a').forEach(item => {
+    item.addEventListener('click', function() {
+        document.getElementById('sidebar').classList.remove('show');
+    });
+});
