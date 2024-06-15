19589a1a3b961181931d7bd4440d4d402ca2f737
purna9547 <pradhan.purna2017@gmail.com>
2024-06-16 01:07:41 +0530

    update fragment of doctor

--- src/main/resources/static/js/Doctor/fragments.js
+++ src/main/resources/static/js/Doctor/fragments.js
@@ -1,3 +1,15 @@
 function logout() {
     window.location.href = "/logout";
 }
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('show');
+});
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.getElementById('sidebar').classList.toggle('show');
+});
+
+document.querySelectorAll('.sidebar a').forEach(item => {
+    item.addEventListener('click', function() {
+        document.getElementById('sidebar').classList.remove('show');
+    });
+});
